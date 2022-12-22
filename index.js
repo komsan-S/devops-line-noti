9b634e1f7a3f7873417176d07e89dc31893b387b
GMMGRAMMY\KomsanSom <komsan.som@gmmgrammy.com>
2022-12-22 16:30:44 +0700

    test

--- index.js
+++ index.js
@@ -12,33 +12,34 @@ app.post('/sent-to-linenoti',(req,res,next) => {
     const payload = req.body
     // console.log("payload", payload)
     console.log(JSON.stringify(payload.detailedMessage.text))
+    message = JSON.stringify(payload.detailedMessage.text)
     // console.log(JSON.stringify(payload.detailedMessage["text"]))
 const response = request({
     method: 'POST',
     uri: 'https://notify-api.line.me/api/notify',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     },
     auth: {
-      'bearer': 'd8cFwcoMGf8Ym5GRQIwjLHojEVh9Powd91gkEGk0CvF'
+        'bearer': 'd8cFwcoMGf8Ym5GRQIwjLHojEVh9Powd91gkEGk0CvF'
     },
     form: {
-      message: JSON.stringify(payload.detailedMessage.text)
+        message: "โง่โง่ๆ"
     }
-  }, (err, httpResponse, body) => {
-    if(err){
-      console.log(err);
+}, (err, httpResponse, body) => {
+    if (err) {
+        console.log(err);
     } else {
         console.log({
             httpResponse: httpResponse,
             body: body
-          });
-      res.json({
-        httpResponse: httpResponse,
-        body: body
-      });
+        });
+        res.json({
+            httpResponse: httpResponse,
+            body: body
+        });
     }
-  });
+});
     res.send(response);
 })
 app.listen(process.env.PORT || 3000,() => {
