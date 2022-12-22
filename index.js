10aeb13030aa49eea86a4dac43420949980972b4
GMMGRAMMY\KomsanSom <komsan.som@gmmgrammy.com>
2022-12-22 15:51:34 +0700

    edit code send noti

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const axios = require('axios');
+var request = require('request');
 const app = express()
 
 
@@ -15,28 +15,28 @@ app.all('/', (req, res) => {
 
 app.post('/sent-to-linenoti',(req,res,next) => {
     const payload = req.body
-    console.log("payload : "+payload);
-    console.log("message : "+JSON.stringify(payload.message));
-    data = {"status":200,"message":JSON.stringify(payload.message)}
-    
-    console.log("data : "+data);
-const config = {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-    auth : {
-        bearer :'d8cFwcoMGf8Ym5GRQIwjLHojEVh9Powd91gkEGk0CvF'
-    }
-  };
 
-    const response = axios.post('https://notify-api.line.me/api/notify', data,config)
-  .then(response => {
-    // console.log(response.data);
-    return response.data
-  })
-  .catch(error => {
-    // console.log(error);
-    return error
+const response = request({
+    method: 'POST',
+    uri: 'https://notify-api.line.me/api/notify',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      'bearer': 'd8cFwcoMGf8Ym5GRQIwjLHojEVh9Powd91gkEGk0CvF'
+    },
+    form: {
+      message: JSON.stringify(payload.message)
+    }
+  }, (err, httpResponse, body) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.json({
+        httpResponse: httpResponse,
+        body: body
+      });
+    }
   });
     res.send(response);
 })
