61a0820d485ca5c617a11dea8e0f7f62ddd3f88d
GMMGRAMMY\KomsanSom <komsan.som@gmmgrammy.com>
2022-12-22 14:58:48 +0700

    add api

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+
+app.post('/sent-to-linenoti',(req,res,next) => {
+    const payload = req.body
+    console.log(payload);
+
+    res.send(200);
+})
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
+})
