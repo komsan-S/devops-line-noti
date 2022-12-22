a5cf42c5119cbab2c0da017922f39e7195ceb116
GMMGRAMMY\KomsanSom <komsan.som@gmmgrammy.com>
2022-12-22 15:32:54 +0700

    add line noti

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express()
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,7 +17,24 @@ app.post('/sent-to-linenoti',(req,res,next) => {
     const payload = req.body
     console.log("payload : "+payload);
 
-    res.send(payload);
+    
+const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Bearer d8cFwcoMGf8Ym5GRQIwjLHojEVh9Powd91gkEGk0CvF'
+    }
+  };
+
+    const response = axios.post('https://notify-api.line.me/api/notify', payload)
+  .then(response => {
+    // console.log(response.data);
+    return response.data
+  })
+  .catch(error => {
+    // console.log(error);
+    return error
+  });
+    res.send(response);
 })
 app.listen(process.env.PORT || 3000,() => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`)
